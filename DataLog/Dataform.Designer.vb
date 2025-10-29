2e97d59907f0fd7900f1690576ec9fef05e57de2
Payden Hoskins <paydenhoskins@gmail.com>
2025-10-29 13:24:50 -0600

    ability to draw a line

--- DataLog/Dataform.Designer.vb
+++ DataLog/Dataform.Designer.vb
@@ -22,10 +22,132 @@ Partial Class Dataform
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GraphPictureBox = New System.Windows.Forms.PictureBox()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.GraphButton = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ButtonGroupBox.SuspendLayout()
+        Me.TopMenuStrip.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GraphPictureBox
+        '
+        Me.GraphPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GraphPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.GraphPictureBox.Location = New System.Drawing.Point(12, 27)
+        Me.GraphPictureBox.Name = "GraphPictureBox"
+        Me.GraphPictureBox.Size = New System.Drawing.Size(776, 271)
+        Me.GraphPictureBox.TabIndex = 0
+        Me.GraphPictureBox.TabStop = False
+        '
+        'ButtonGroupBox
+        '
+        Me.ButtonGroupBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
+        Me.ButtonGroupBox.Controls.Add(Me.GraphButton)
+        Me.ButtonGroupBox.Controls.Add(Me.Button1)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(400, 349)
+        Me.ButtonGroupBox.Name = "ButtonGroupBox"
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(400, 76)
+        Me.ButtonGroupBox.TabIndex = 1
+        Me.ButtonGroupBox.TabStop = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(272, 19)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(104, 44)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'GraphButton
+        '
+        Me.GraphButton.Location = New System.Drawing.Point(149, 19)
+        Me.GraphButton.Name = "GraphButton"
+        Me.GraphButton.Size = New System.Drawing.Size(104, 44)
+        Me.GraphButton.TabIndex = 1
+        Me.GraphButton.Text = "&Graph"
+        Me.GraphButton.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(24, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(800, 24)
+        Me.TopMenuStrip.TabIndex = 2
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(120, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'Dataform
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ButtonGroupBox)
+        Me.Controls.Add(Me.GraphPictureBox)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Name = "Dataform"
         Me.Text = "Form1"
+        CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ButtonGroupBox.ResumeLayout(False)
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GraphPictureBox As PictureBox
+    Friend WithEvents ButtonGroupBox As GroupBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents GraphButton As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
 End Class
--- DataLog/Dataform.vb
+++ DataLog/Dataform.vb
@@ -1,3 +1,55 @@
-﻿Public Class Dataform
+﻿Option Explicit On
+Option Strict On
 
+Public Class Dataform
+    '---------------------------------- Program Logic ------------------------------------
+    Function GetRandomNumberAround(thisNumber%, Optional within% = 10) As Integer
+        Dim result%
+
+        result = GetRandomNumber(within) + GetRandomNumber(within)
+
+        Return result
+    End Function
+
+    Function GetRandomNumber(max%) As Integer
+        Randomize()
+
+        Return CInt(System.Math.Floor((Rnd() * (max + 1))))
+    End Function
+
+
+    Function GetData() As Integer
+        Return 5
+    End Function
+
+    Sub GraphData()
+
+        Dim g As Graphics = GraphPictureBox.CreateGraphics
+        Dim pen As Pen
+        pen.Color = Color.BlanchedAlmond
+        Dim scaleX As Single = CSng(GraphPictureBox.Width / 100)
+        Dim scaleY As Single = CSng((GraphPictureBox.Height / 100) * -1)
+
+        g.TranslateTransform(0, GraphPictureBox.Height)
+        g.ScaleTransform(scaleX, scaleY)
+
+
+
+        g.DrawLine(pen, 5, 5, 95, 5)
+
+        g.Dispose()
+        pen.Dispose()
+
+    End Sub
+
+    '----------------------------------- Event handlers ----------------------------------
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub GraphButton_Click(sender As Object, e As EventArgs) Handles GraphButton.Click
+        For i = 1 To 100
+            GetRandomNumberAround(50)
+        Next
+    End Sub
 End Class
